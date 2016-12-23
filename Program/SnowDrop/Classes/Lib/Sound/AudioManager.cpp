6a4aad22fc554a76273dabdb770e2caf3ba3ed32
小雪 山崎 <gsk.voice@gmail.com>
2016-12-23 09:18:07 +0900

    AudioManager.h/.cppの移動 Title→AudioManager.h/.cpp Appdelegate.cpp内のSimpleAudioEngine削除

---
+++ Program/SnowDrop/Classes/Lib/Sound/AudioManager.cpp
@@ -0,0 +1,58 @@
+
+//==========================================
+//
+// File: AudioManager.cpp
+//
+// 音楽マネージャーファイル
+//
+// 2016/12/23
+//						Author Koyuki Yamasaki
+//
+//==========================================
+
+//==========================================
+// ヘッダインクルード
+//==========================================
+#include "AudioManager.h"
+
+
+// 共有インスタンス
+CAudioManager* CAudioManager::m_pSharedAudioManager = NULL;
+
+/*
+*	@desc	フェードイン
+*	@param	対象音楽の名称
+*/
+void CAudioManager::fadeIn(std::string musicName) {
+
+	int musicID = this->getMusicID(musicName);
+
+	float volume = AudioEngine::getVolume(musicID);
+
+	// MAX Volume 出なければ足す
+	if (volume < 1.0f) {
+		AudioEngine::setVolume(musicID, volume + 0.01f);
+	}
+
+}
+
+/*
+*	@desc	フェードアウト
+*	@param	対象音楽の名称
+*/
+void CAudioManager::fadeOut(std::string musicName) {
+
+	int musicID = this->getMusicID(musicName);
+
+	float volume = AudioEngine::getVolume(musicID);
+
+	// MAX Volume 出なければ足す
+	if (volume > 0.0f) {
+		AudioEngine::setVolume(musicID, volume - 0.01f);
+	}
+	else {
+		// 音楽を止める
+		AudioEngine::stop(musicID);
+	}
+
+}
--- Program/SnowDrop/Classes/Scene/Title/Title.cpp
+++ Program/SnowDrop/Classes/Scene/Title/Title.cpp
@@ -14,7 +14,7 @@
 #include"Title.h"
 #include"Constants.h"
 #include "Scene\GameMain\GameMain.h"
-#include "audio/include/AudioEngine.h"
+#include "Lib/Sound/AudioManager.h"
 using namespace experimental;
 
 /*
@@ -181,46 +181,6 @@ void CTitle::callbackEndGame(cocos2d::Ref* pSender)
 
 
 
-// 共有インスタンス
-CAudioManager* CAudioManager::m_pSharedAudioManager = NULL;
-
-/*
-*	@desc	フェードイン
-*	@param	対象音楽の名称
-*/
-void CAudioManager::fadeIn(std::string musicName) {
-
-	int musicID = this->getMusicID(musicName);
-
-	float volume = AudioEngine::getVolume(musicID);
-
-	// MAX Volume 出なければ足す
-	if (volume < 1.0f) {
-		AudioEngine::setVolume(musicID, volume + 0.01f);
-	}
-
-}
-
-/*
-*	@desc	フェードアウト
-*	@param	対象音楽の名称
-*/
-void CAudioManager::fadeOut(std::string musicName) {
-
-	int musicID = this->getMusicID(musicName);
-
-	float volume = AudioEngine::getVolume(musicID);
-
-	// MAX Volume 出なければ足す
-	if (volume > 0.0f) {
-		AudioEngine::setVolume(musicID, volume - 0.01f);
-	}
-	else{
-		// 音楽を止める
-		AudioEngine::stop(musicID);
-	}
-
-}
 
 
 //EOF
